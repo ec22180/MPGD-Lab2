fa4925857d4a45b1e9e41361b66e5c13bb5a85a6
ec22180 <agra.vasundhara@gmail.com>
2022-12-14 18:07:15 +0000

    final code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,25 +8,52 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     private int count;
-    private int numPickups = 10;
+    private int numPickups = 5;
     public Text scoreText;
     public Text winText;
     public Text playerPosition;
+    private Rigidbody rb;
+    private Vector3 prePos = Vector3.zero;
+    private float objSpeed;
+    public Text playerSpeed;
+    public GameObject[] PickupArr;
+    public float minDis = 2f;
 
     void Start()
     {
         count = 0;
         winText.text = "";
         SetCountText();
+        rb = GetComponent<Rigidbody>();
+        prePos = transform.position;
+        
     }
     void Update()
     {
         float xDirection = Input.GetAxis("Horizontal");
         float zDirection = Input.GetAxis("Vertical");
+        rb.AddForce(xDirection * speed * Time.fixedDeltaTime, rb.velocity.y, zDirection * speed * Time.fixedDeltaTime);
+        
+        foreach(GameObject go in PickupArr)
+        {
+            float distance = Vector3.Distance(go.transform.position , transform.position);
 
-        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
-        transform.position += moveDirection;
-        playerPosition.text = "X: " + gameObject.transform.position.x +" "+ "Y: " + gameObject.transform.position.y + " " + "Z: " + gameObject.transform.position.z;
+            if(distance<minDis)
+            {
+                go.GetComponent<Renderer>().material.color = new Color(0,0,255);
+            }
+            else if (distance>minDis)
+            {
+                go.GetComponent<Renderer>().material.color = new Color(0.85f, 0.807f, 0.298f);
+            }
+        }
+       
+    }
+    public void FixedUpdate()
+    {
+        objSpeed = (transform.position - prePos).magnitude / Time.deltaTime;
+        prePos = transform.position;
+        playerSpeed.text = "Velocity: " + objSpeed.ToString("0.00");
     }
 
     void OnTriggerEnter(Collider other)
